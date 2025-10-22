ae559a8ecbfb168b34ccb64a4ffc0967c6148793
GLD5000 <garethlouisdevlin@gmail.com>
2025-10-22 14:49:08 +0100

    Add Jest

---
+++ jest.setup.js
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+
+function MockImage(props) {
+    return React.createElement('img', props);
+}
+jest.mock('next/image', () => MockImage);
